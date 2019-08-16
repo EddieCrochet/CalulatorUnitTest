149f6a1caf43bdcebfbbcdbb7445d6e20d308120
Eddie Crochet <eddiecrochet1994@gmail.com>
2019-08-15 21:53:45 -0500

    Added class to do basic calculations

--- CalulatorUnitTest.Services/Class1.cs
+++ CalulatorUnitTest.Services/Class1.cs
@@ -2,7 +2,16 @@
 
 namespace CalulatorUnitTest.Services
 {
-    public class Class1
+    public class Calculator
     {
+        public int Add(int num1, int num2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Sub(int num1, int num2)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
